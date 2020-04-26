ff2d561c0fc4fab96d0a1313a6024b994622edff
Mykhailo Savchenko <mykhailo.savchenko@avenga.com>
2020-04-26 11:32:43 +0300

    fix Set Issues

--- src/main/java/com/mihadev/zebra/entity/schedule/ScheduleClass.java
+++ src/main/java/com/mihadev/zebra/entity/schedule/ScheduleClass.java
@@ -1,12 +1,12 @@
 package com.mihadev.zebra.entity.schedule;
 
-import com.fasterxml.jackson.annotation.JsonIgnore;
 import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
 import com.mihadev.zebra.entity.ClassType;
 import com.mihadev.zebra.entity.Coach;
 
 import javax.persistence.*;
 import java.time.LocalTime;
+import java.util.Objects;
 
 @Entity
 public class ScheduleClass {
@@ -71,5 +71,22 @@ public class ScheduleClass {
 
     public void setCoach(Coach coach) {
         this.coach = coach;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        ScheduleClass that = (ScheduleClass) o;
+        return Objects.equals(id, that.id) &&
+                Objects.equals(startTime, that.startTime) &&
+                classType == that.classType &&
+                Objects.equals(scheduleDay, that.scheduleDay) &&
+                Objects.equals(coach, that.coach);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(id, startTime, classType, scheduleDay, coach);
     }
 }
