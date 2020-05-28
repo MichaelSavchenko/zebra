722d23913a2400e4e01e412482600ee18d05349a
Mykhailo Savchenko <mykhailo.savchenko@avenga.com>
2020-05-28 22:13:37 +0300

    --

--- src/main/java/com/mihadev/zebra/Application.java
+++ src/main/java/com/mihadev/zebra/Application.java
@@ -1,6 +1,10 @@
 package com.mihadev.zebra;
 
+import com.mihadev.zebra.entity.Role;
+import com.mihadev.zebra.entity.User;
+import com.mihadev.zebra.repository.RoleRepository;
 import com.mihadev.zebra.repository.UserRepository;
+import com.mihadev.zebra.service.UserService;
 import com.mihadev.zebra.startscripts.SetupAbonClasses;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
@@ -11,6 +15,8 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.Bean;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
+
+import java.util.Collections;
 
 @SpringBootApplication
 public class Application {
@@ -33,10 +39,10 @@ public class Application {
     }
 
     @Bean
-    public CommandLineRunner demo() {
+    public CommandLineRunner demo(SetupAbonClasses setupAbonClasses) {
         return args -> {
             System.out.println("Started");
-
+                setupAbonClasses.execute();
             System.out.println("finished");
         };
     }
