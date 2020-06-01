922fbece6b69bb019a6fc446784df9f0507e538f
Mykhailo Savchenko <mykhailo.savchenko@avenga.com>
2020-06-01 22:40:12 +0300

    remove cache

--- src/main/java/com/mihadev/zebra/Application.java
+++ src/main/java/com/mihadev/zebra/Application.java
@@ -43,7 +43,6 @@ public class Application {
     public CommandLineRunner demo(SetupAbonClasses setupAbonClasses) {
         return args -> {
             System.out.println("Started");
-                setupAbonClasses.execute();
             System.out.println("finished");
         };
     }
