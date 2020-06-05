61a5d97753a8361c0febd4fd93ab4854fb4d4325
Mykhailo Savchenko <mykhailo.savchenko@avenga.com>
2020-06-05 21:21:03 +0300

    try to avoid memory leak

--- src/main/java/com/mihadev/zebra/entity/User.java
+++ src/main/java/com/mihadev/zebra/entity/User.java
@@ -18,7 +18,7 @@ public class User {
     private String lastName;
     private String password;
 
-    @ManyToMany
+    @ManyToMany(fetch = FetchType.EAGER)
     @JoinTable(name = "user_roles",
     joinColumns = {@JoinColumn(name = "user_id", referencedColumnName = "id")},
     inverseJoinColumns = {@JoinColumn(name = "role_id", referencedColumnName = "id")})
