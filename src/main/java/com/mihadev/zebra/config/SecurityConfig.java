715a60efc4d59a3cb636c0431cdbe28378750d5e
Mykhailo Savchenko <mykhailo.savchenko@avenga.com>
2020-07-14 17:55:53 +0300

    try to setup cors

--- src/main/java/com/mihadev/zebra/config/SecurityConfig.java
+++ src/main/java/com/mihadev/zebra/config/SecurityConfig.java
@@ -6,10 +6,11 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 import org.springframework.security.config.http.SessionCreationPolicy;
 
-@Configuration
+@EnableWebSecurity
 public class SecurityConfig extends WebSecurityConfigurerAdapter {
 
     private final JWTTokenProvider jwtTokenProvider;
@@ -28,6 +29,7 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
     @Override
     protected void configure(HttpSecurity http) throws Exception {
         http
+                .cors().and()
                 .httpBasic().disable()
                 .csrf().disable()
                 .sessionManagement().sessionCreationPolicy(SessionCreationPolicy.STATELESS)
@@ -35,7 +37,17 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
                 .authorizeRequests()
                 .antMatchers("/login").permitAll()
                 .antMatchers("/salary", "/price").hasAnyRole("ADMIN")
-                .anyRequest().permitAll()
+                .antMatchers(
+                        "/abons/**",
+                        "/classes/**",
+                        "/coach-app/**",
+                        "coaches/**",
+                        "/scheduleClass/**",
+                        "/schedule/**",
+                        "/students/**",
+                        "telegram/**"
+                ).hasAnyRole("ADMIN", "COACH")
+                .anyRequest().authenticated()
                 .and()
                 .apply(new JWTConfigurer(jwtTokenProvider));
     }
