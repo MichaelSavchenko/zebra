7be6c70bf984e377e5f03414f8b567162978f7c5
Mykhailo Savchenko <mykhailo.savchenko@avenga.com>
2020-07-14 17:22:38 +0300

    try to setup cors

--- src/main/java/com/mihadev/zebra/config/SecurityConfig.java
+++ src/main/java/com/mihadev/zebra/config/SecurityConfig.java
@@ -6,12 +6,10 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
-import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 import org.springframework.security.config.http.SessionCreationPolicy;
 
 @Configuration
-@EnableWebSecurity
 public class SecurityConfig extends WebSecurityConfigurerAdapter {
 
     private final JWTTokenProvider jwtTokenProvider;
