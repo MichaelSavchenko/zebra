99ce81e854eb0ec12725f06d7f7557abd47c5c20
Mykhailo Savchenko <mykhailo.savchenko@avenga.com>
2020-10-14 18:12:38 +0300

    --

--- src/main/java/com/mihadev/zebra/Application.java
+++ src/main/java/com/mihadev/zebra/Application.java
@@ -47,7 +47,6 @@ public class Application {
     public CommandLineRunner demo(SetupAbonClasses setupAbonClasses, UserService userService, AbonClazzRepository abonClazzRepository, ScheduleScript scheduleScript) {
         return args -> {
             System.out.println("Started");
-            scheduleScript.setupSchedule();
             System.out.println("finished");
         };
     }
