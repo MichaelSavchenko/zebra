82e6ce1b90a9c034eb98a0d17219071342a3d2cc
mihadev <michaelsav4enko@gmail.com>
2021-02-28 19:12:22 +0200

    fix fetching abons on delete

--- src/main/java/com/mihadev/zebra/service/AbonService.java
+++ src/main/java/com/mihadev/zebra/service/AbonService.java
@@ -203,7 +203,7 @@ public class AbonService {
         List<Integer> deleted = new ArrayList<>();
 
         for (Student student : students) {
-            List<Abon> abons = new ArrayList<>(student.getAbons());
+            List<Abon> abons = abonRepository.findByStudents(student);
             forUpdate.addAll(abons);
 
             for (Abon abon : abons) {
