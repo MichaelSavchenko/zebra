bee26a0f6267a24ff738b4e193a6f2d1dc0620a4
mihadev <michaelsav4enko@gmail.com>
2021-04-14 18:27:34 +0300

    test eager fetch for students on abons

--- src/main/java/com/mihadev/zebra/service/AbonService.java
+++ src/main/java/com/mihadev/zebra/service/AbonService.java
@@ -90,34 +90,17 @@ public class AbonService {
 
     private void checkMultiplyActiveAbons(List<Abon> abons) {
         long start = System.currentTimeMillis();
-        /*Map<Integer, List<Abon>> studentAbons = abons.stream()
+        Map<Integer, List<Abon>> studentAbons = abons.stream()
                 .collect(Collectors.groupingBy(abon ->
                         abon.getStudents().stream()
                                 .findFirst()
                                 .map(Student::getId)
-                                .orElse(-1)));*/
-
-        Map<Integer, Set<Abon>> test = new HashMap<>();
-
-        for (Abon abon: abons) {
-            Set<Student> students = abon.getStudents();
-
-            for (Student student: students) {
-                Set<Abon> abonList = test.get(student.getId());
-                if (Objects.nonNull(abonList)) {
-                    abonList.add(abon);
-                } else {
-                    abonList = new HashSet<>();
-                    abonList.add(abon);
-                    test.put(student.getId(), abonList);
-                }
-            }
-        }
+                                .orElse(-1)));
 
         System.out.println("mapping Map<Integer, List<Abon>>:" + (System.currentTimeMillis() - start));
 
-        for (Set<Abon> abonsOfSingleStudent : test.values()) {
-            setActiveAbons(abonsOfSingleStudent);
+        for (List<Abon> abonsOfSingleStudent : studentAbons.values()) {
+            setActiveAbons(new HashSet<>(abonsOfSingleStudent));
         }
     }
 
