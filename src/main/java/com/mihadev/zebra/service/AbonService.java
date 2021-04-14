1f0774f3f28f77b81445f0ad23e2f8e8354cf4b1
mihadev <michaelsav4enko@gmail.com>
2021-04-14 17:50:12 +0300

    debug checkMultiplyActiveAbons -- use Id instead of Student

--- src/main/java/com/mihadev/zebra/service/AbonService.java
+++ src/main/java/com/mihadev/zebra/service/AbonService.java
@@ -90,12 +90,13 @@ public class AbonService {
 
     private void checkMultiplyActiveAbons(List<Abon> abons) {
         long start = System.currentTimeMillis();
-        Map<Student, List<Abon>> studentAbons = abons.stream()
+        Map<Integer, List<Abon>> studentAbons = abons.stream()
                 .collect(Collectors.groupingBy(abon ->
                         abon.getStudents().stream()
                                 .findFirst()
-                                .orElse(new Student())));
-        System.out.println("mapping Map<Student, List<Abon>>:" + (System.currentTimeMillis() - start));
+                                .map(Student::getId)
+                                .orElse(-1)));
+        System.out.println("mapping Map<Integer, List<Abon>>:" + (System.currentTimeMillis() - start));
 
         for (List<Abon> abonsOfSingleStudent : studentAbons.values()) {
             setActiveAbons(new HashSet<>(abonsOfSingleStudent));
