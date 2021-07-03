fc22c6bdc07cd151946bba48cd6fedfb4e7b1ee2
mihadev <michaelsav4enko@gmail.com>
2021-07-03 14:35:10 +0300

    delete students

--- src/main/java/com/mihadev/zebra/Application.java
+++ src/main/java/com/mihadev/zebra/Application.java
@@ -51,6 +51,7 @@ public class Application {
             PriceRepository priceRepository) {
         return args -> {
             System.out.println("Started");
+            studentRepository.deleteEmptyStudents();
             System.out.println("finished");
         };
     }
