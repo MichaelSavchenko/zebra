bc8f7d52a4327dcff452fb8f35c3290cd4a9adde
mihadev <michaelsav4enko@gmail.com>
2022-01-10 17:59:50 +0200

    fix creation of new column

--- src/main/java/com/mihadev/zebra/entity/Price.java
+++ src/main/java/com/mihadev/zebra/entity/Price.java
@@ -1,5 +1,6 @@
 package com.mihadev.zebra.entity;
 
+import javax.persistence.Column;
 import javax.persistence.Entity;
 import javax.persistence.Id;
 
@@ -7,7 +8,9 @@ import javax.persistence.Id;
 public class Price {
     @Id
     private ClassType classType;
+    @Column(columnDefinition = "integer default 0")
     private int costPerStudent;
+    @Column(columnDefinition = "integer default 0")
     private int costPerClass;
 
     public ClassType getClassType() {
