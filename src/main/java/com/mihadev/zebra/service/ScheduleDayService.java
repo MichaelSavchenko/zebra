ab55ab3055e38b07869751e02980f73a6c410a89
Mike <michaelsav4enko@gmail.com>
2024-04-01 11:19:25 +0300

    fix build

--- src/main/java/com/mihadev/zebra/service/ScheduleDayService.java
+++ src/main/java/com/mihadev/zebra/service/ScheduleDayService.java
@@ -3,9 +3,11 @@ package com.mihadev.zebra.service;
 import com.mihadev.zebra.entity.schedule.Schedule;
 import com.mihadev.zebra.entity.schedule.ScheduleDay;
 import com.mihadev.zebra.repository.ScheduleDayRepository;
+import org.springframework.stereotype.Service;
 
 import java.time.DayOfWeek;
 
+@Service
 public class ScheduleDayService {
 
     private final static int MYTNYTSYA_SCHEDULE = 740782;
