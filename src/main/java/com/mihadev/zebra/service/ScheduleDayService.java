092b5abf6bf4303be8bf969bf7f40f20c0c936d8
Mike <michaelsav4enko@gmail.com>
2024-04-01 11:26:37 +0300

    fix save of the day

--- src/main/java/com/mihadev/zebra/service/ScheduleDayService.java
+++ src/main/java/com/mihadev/zebra/service/ScheduleDayService.java
@@ -3,6 +3,7 @@ package com.mihadev.zebra.service;
 import com.mihadev.zebra.entity.schedule.Schedule;
 import com.mihadev.zebra.entity.schedule.ScheduleDay;
 import com.mihadev.zebra.repository.ScheduleDayRepository;
+import com.mihadev.zebra.repository.ScheduleRepository;
 import org.springframework.stereotype.Service;
 
 import java.time.DayOfWeek;
@@ -13,14 +14,16 @@ public class ScheduleDayService {
     private final static int MYTNYTSYA_SCHEDULE = 740782;
 
     private final ScheduleDayRepository scheduleDayRepository;
+    private final ScheduleRepository scheduleRepository;
 
-    public ScheduleDayService(ScheduleDayRepository scheduleDayRepository) {
+    public ScheduleDayService(ScheduleDayRepository scheduleDayRepository, ScheduleRepository scheduleRepository) {
         this.scheduleDayRepository = scheduleDayRepository;
+        this.scheduleRepository = scheduleRepository;
     }
 
     public void saveSunday() {
-        Schedule schedule = new Schedule();
-        schedule.setId(MYTNYTSYA_SCHEDULE);
+        Schedule schedule = scheduleRepository.findById(MYTNYTSYA_SCHEDULE)
+                .orElseThrow(() -> new RuntimeException("Schedule not found"));
         ScheduleDay sunday = new ScheduleDay(DayOfWeek.SUNDAY, schedule);
         scheduleDayRepository.save(sunday);
     }
