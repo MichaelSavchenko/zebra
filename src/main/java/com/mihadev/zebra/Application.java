0c877861e2271907f4c95ca56162cb199d4b736f
Mike <michaelsav4enko@gmail.com>
2024-09-22 20:55:33 +0300

    update tel number

--- src/main/java/com/mihadev/zebra/Application.java
+++ src/main/java/com/mihadev/zebra/Application.java
@@ -54,6 +54,14 @@ public class Application {
         return args -> {
             System.out.println("Started");
 
+            User byUserName = userRepository.findByUserName("+380639318563");
+            if (byUserName != null) {
+                byUserName.setUserName("+380639318562");
+                userRepository.save(byUserName);
+
+                System.out.println("Success +380639318562");
+            }
+
             System.out.println("finished");
         };
     }
